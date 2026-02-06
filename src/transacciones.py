a67428d125e6533e4db38d58dc29a7d173f44fe2
dpareja1 <danielparejaperez@gmail.com>
2026-02-06 09:54:14 -0500

    Update transacciones.py

--- src/transacciones.py
+++ src/transacciones.py
@@ -97,23 +97,6 @@ def procesar_transacciones(ruta_csv, df_inventario, df_feedback):
         how='left'                                     # Left join
     )
 
-    cols_texto = df_trans.select_dtypes(include=['object', 'string']).columns
-         
-    for col in cols_texto:
-        df_trans[col] = df_trans[col].fillna('').astype(str)
-        # A. Convertir a minúsculas
-        df_trans[col] = df_trans[col].str.lower()
-        
-        # B. Eliminar tildes (Normalización Unicode)
-        # Explicación técnica: 
-        # 'NFKD' separa la letra de la tilde (á -> a + ´). 
-        # 'encode' elimina los caracteres no ASCII (la tilde suelta).
-        # 'decode' devuelve el texto a string legible.
-        df_trans[col] = df_trans[col].str.normalize('NFKD')\
-                         .str.encode('ascii', errors='ignore')\
-                         .str.decode('utf-8')
-
-    
     # ==========================================
     # PASO 10: CREACIÓN DE IDENTIFICADOR GRUPAL
     # ==========================================
@@ -203,6 +186,7 @@ def procesar_transacciones(ruta_csv, df_inventario, df_feedback):
   
 
     return df_trans, metricas
+
 
 
 
